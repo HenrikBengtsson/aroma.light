ef5553db074f85b024e0f531669f9eea084c56b6
Henrik Bengtsson <hb@aroma-project.org>
2007-11-28 22:54:24 +0000

    Version: 1.7.1 [2007-11-28] o The startup message when loading the package is generated with   packageStartupMessage() so that it can be suppressed. o Package now only suggest the R.oo package, and instead depends   on the new R.methodsS3. o TYPO: Corrected a spelling error in the help pages. o Package passes R CMD check R v2.6.1.
    Version: 1.7.0 [2007-10-08]
    o The version number was bumped for the Bioconductor devel version.
    
    Version: 1.6.0 [2007-10-08]
    o The version number was bumped for the Bioconductor release version.

--- R/zzz.R
+++ R/zzz.R
@@ -4,8 +4,12 @@
 
 
 .onAttach <- function(libname, pkgname) {
-  pkg <- Package(pkgname);
-  assign(pkgname, pkg, pos=getPosition(pkg));
-  cat(getName(pkg), " v", getVersion(pkg), " (", getDate(pkg), ")",
-      " successfully loaded. See ?", pkgname, " for help.\n", sep="");
+  if (require("R.oo")) {
+    pkg <- Package(pkgname);
+    assign(pkgname, pkg, pos=getPosition(pkg));
+  }
+
+  pi <- utils::packageDescription(pkgname);
+  packageStartupMessage(pkgname, " v", pi$Version, " (", 
+    pi$Date, ") successfully loaded. See ?", pkgname, " for help."); 
 }
