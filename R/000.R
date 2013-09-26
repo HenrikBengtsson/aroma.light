e809e85883ef8a3d91c622c8add9dbfdd568da83
H Bengtsson <hb@aroma-project.org>
2013-09-26 07:00:25 +0000

    # tweak tweak
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/aroma.light@80784 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/000.R
+++ R/000.R
@@ -9,6 +9,3 @@ autoload("appendVarArgs", package="R.methodsS3")
 autoload("hasVarArgs", package="R.methodsS3")
 autoload("setMethodS3", package="R.methodsS3")
 autoload("setConstructorS3", package="R.methodsS3")
-
-
-
